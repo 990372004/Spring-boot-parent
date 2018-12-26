d8565eab493a0932cc0fac57b05b057496f2da6b
chenGang990372004 <990372004@qq.com>
2018-12-26 10:41:43 +0800

    发送邮件  启动类

--- SpringBoot-email/src/main/java/com/chen/SpringBootEmailApplication.java
+++ SpringBoot-email/src/main/java/com/chen/SpringBootEmailApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootEmailApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootEmailApplication.class, args);
 	}
 
