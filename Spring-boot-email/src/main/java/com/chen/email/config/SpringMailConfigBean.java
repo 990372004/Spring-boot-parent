622b0812e5a2a1bfeaa4f45358843f777fceffe2
chenGang990372004 <990372004@qq.com>
2018-12-26 15:19:02 +0800

    通过注解读取非默认的.properties文件内容

---
+++ Spring-boot-email/src/main/java/com/chen/email/config/SpringMailConfigBean.java
@@ -0,0 +1,39 @@
+package com.chen.email.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+@PropertySource("classpath:application-email.properties")
+public class SpringMailConfigBean {
+	private String host;
+	private String username;
+	private String password;
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
