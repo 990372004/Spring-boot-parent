155cde12cd2aa8e0caac40600cf2263e0c333627
990372004 <990372004@qq.com>
2018-12-29 10:19:09 +0800

    spring boot redis  启动类

---
+++ Spring-boot-redis/src/main/java/com/chen/redis/SpringBootRedisApplication.java
@@ -0,0 +1,18 @@
+package com.chen.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class SpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisApplication.class, args);
+	}
+
+}
+
