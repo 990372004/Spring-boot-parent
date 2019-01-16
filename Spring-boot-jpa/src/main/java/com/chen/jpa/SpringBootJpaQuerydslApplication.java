42b34b983aa31c1a21d785867397e08ca6032afc
990372004 <990372004@qq.com>
2019-01-16 17:14:24 +0800

    spring boot jpa 启动程序

---
+++ Spring-boot-jpa/src/main/java/com/chen/jpa/SpringBootJpaQuerydslApplication.java
@@ -0,0 +1,14 @@
+package com.chen.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaQuerydslApplication.class, args);
+	}
+
+}
+
